c8166fa082bc013967e37883fe2870e744d215c9
unknown <dennisndegwa001@gmail.com>
2021-11-03 00:50:29 +0300

    Latest

--- erp/settings.py
+++ erp/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import os
 
+import django_heroku
+
 from datetime import timedelta
 
 from django.conf import settings
@@ -210,3 +212,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+
+django_heroku.settings(locals())
